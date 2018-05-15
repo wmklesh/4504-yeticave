3718e5ecf24b94a331124c0645a01f03449f722e
Alexander Zimin <klesh2@mail.ru>
2018-05-15 16:43:57 +0300

    Функции получения данных

--- functions.php
+++ functions.php
@@ -20,8 +20,47 @@ function includeTemplate($tpl, $data) {
     return '';
 }
 
-function formatLotTimer ($endTime) {
+function formatLotTimer($endTime) {
     $endTime = is_int($endTime)?: strtotime($endTime);
     $time = $endTime - time();
     return sprintf('%02d:%02d', ($time / 3600) % 24, ($time / 60) % 60);
+}
+
+function processQuery($connection, string $sql, array $param = []) {
+    if (empty($param)) {
+        $query = mysqli_query($connection, $sql);
+    }
+
+    return mysqli_fetch_all($query, MYSQLI_ASSOC);
+}
+
+function addLimit(int $limit) {
+    return ' LIMIT ' . $limit;
+}
+
+function getLotList($connection, int $limit = 0) {
+    $sql = 'SELECT l.name, l.price, img, end_time, c.name category_name, IFNULL(b.count_bet, 0) count_bet
+            FROM lot l
+              JOIN category c ON c.id = l.category_id
+              LEFT JOIN
+                (SELECT b.lot_id, COUNT(b.id) count_bet FROM bet b GROUP BY b.lot_id) b
+              ON b.lot_id = l.id
+            WHERE l.end_time > NOW()
+            ORDER BY l.add_time DESC';
+
+    if ($limit) {
+        $sql .= addLimit($limit);
+    }
+
+    return processQuery($connection, $sql);
+}
+
+function getCatList($connection, int $limit = 0) {
+    $sql = 'SELECT * FROM category';
+
+    if ($limit) {
+        $sql .= addLimit($limit);
+    }
+
+    return processQuery($connection, $sql);
 }
--- index.php
+++ index.php
@@ -5,35 +5,25 @@ date_default_timezone_set(‘Europe/Moscow’);
 require __DIR__ . '/functions.php';
 require __DIR__ . '/data.php';
 
-$link = mysqli_connect('localhost', 'root', 'root', 'yeticave');
-mysqli_set_charset($link, 'utf8');
+$connection = mysqli_connect('localhost', 'root', 'root', 'yeticave');
+mysqli_set_charset($connection, 'utf8');
 
-if (! $link) {
+if (! $connection) {
     print('Ошибка: Невозможно подключиться к MySQL  ' . mysqli_connect_error());
+    die();
 }
 
-$sql = 'SELECT l.name, l.price, img, end_time, c.name category_name, IFNULL(b.count_bet, 0) count_bet
-        FROM lot l
-          JOIN category c ON c.id = l.category_id
-          LEFT JOIN
-            (SELECT b.lot_id, COUNT(b.id) count_bet FROM bet b GROUP BY b.lot_id) b
-          ON b.lot_id = l.id
-        WHERE l.end_time > NOW()
-        ORDER BY l.add_time DESC
-        LIMIT 9';
-$lotList = mysqli_query($link, $sql);
-
+$lotList = getLotList($connection, 9);
 $lotListContent = '';
-while ($lot = mysqli_fetch_array($lotList)) {
+foreach ($lotList as $lot) {
     $lotListContent .= includeTemplate('lot', $lot);
 }
+
 $pageContent = includeTemplate('index', ['lotListContent' => $lotListContent]);
 
-$sql = 'SELECT * FROM category';
-$categoryList = mysqli_query($link, $sql);
-
+$categoryList = getCatList($connection);
 $catListContent = '';
-while ($category = mysqli_fetch_array($categoryList)) {
+foreach ($categoryList as $category) {
     $catListContent .= includeTemplate('nav', ['name' => $category['name']]);
 }
 
