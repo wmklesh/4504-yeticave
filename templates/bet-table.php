1f1131de6c50569fb533cd7c2b070dc7cf320bc2
Alexander Zimin <klesh2@mail.ru>
2018-05-17 13:19:17 +0300

    Добработка lot.php

---
+++ templates/bet-table.php
@@ -0,0 +1,5 @@
+<tr class="history__item">
+    <td class="history__name"><?= $name ?></td>
+    <td class="history__price"><?= formatPrice($price) ?></td>
+    <td class="history__time">5 минут назад</td>
+</tr>
--- templates/lot-item.php
+++ templates/lot-item.php
@@ -4,14 +4,14 @@
     </div>
     <div class="lot__info">
         <span class="lot__category"><?= $categoryName ?></span>
-        <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $name ?></a></h3>
+        <h3 class="lot__title"><a class="text-link" href="lot.php?id=<?= $id ?>"><?= $name ?></a></h3>
         <div class="lot__state">
             <div class="lot__rate">
                 <span class="lot__amount">Стартовая цена</span>
                 <span class="lot__cost"><?= formatPrice($price) ?><b class="rub">р</b></span>
             </div>
             <div class="lot__timer timer">
-                <?= formatLotTimer($end_time)?>
+                <?= formatLotTimer($end_time) ?>
             </div>
         </div>
     </div>
