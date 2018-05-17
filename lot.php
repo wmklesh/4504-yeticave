92f448e8accec59e0beca24e8f96a0dd8489359a
Alexander Zimin <klesh2@mail.ru>
2018-05-17 12:21:37 +0300

    Страница lot

--- lot.php
+++ lot.php
@@ -3,5 +3,5 @@
  * Created by PhpStorm.
  * User: klesh
  * Date: 17.05.2018
- * Time: 10:58
+ * Time: 10:52
  */
---
+++ templates/_lot.php
@@ -0,0 +1,18 @@
+<li class="lots__item lot">
+    <div class="lot__image">
+        <img src="<?= $img ?>" width="350" height="260" alt="<?= $name ?>">
+    </div>
+    <div class="lot__info">
+        <span class="lot__category"><?= $categoryName ?></span>
+        <h3 class="lot__title"><a class="text-link" href="lot.html"><?= $name ?></a></h3>
+        <div class="lot__state">
+            <div class="lot__rate">
+                <span class="lot__amount">Стартовая цена</span>
+                <span class="lot__cost"><?= formatPrice($price) ?><b class="rub">р</b></span>
+            </div>
+            <div class="lot__timer timer">
+                <?= formatLotTimer($end_time)?>
+            </div>
+        </div>
+    </div>
+</li>
